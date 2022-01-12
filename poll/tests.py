ad0ccd545709f18f597bf2d484029ba77c58c3dc
gredet200 <gredet200@gmail.com>
2022-01-12 22:10:14 +0200

    Last_working

--- poll/tests.py
+++ poll/tests.py
@@ -6,11 +6,23 @@ import unittest
 
 class TestSum(unittest.TestCase):
 
+
     def test_sum(self):
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
     def test_sum_tuple(self):
         self.assertEqual(sum((1, 2, 2)), 5, "Should be 6")
 
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+	
 if __name__ == '__main__':
     unittest.main()
+	
+	
